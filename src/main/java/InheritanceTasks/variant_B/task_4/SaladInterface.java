dde53d6dd9c2617daa6c614a5fcea098c6b5aa1c
darika123123 <96609893+darika123123@users.noreply.github.com>
2022-10-03 19:50:01 +0600

    committed by darika :fire:

---
+++ src/main/java/InheritanceTasks/variant_B/task_4/SaladInterface.java
@@ -0,0 +1,8 @@
+package InheritanceTasks.variant_B.task_4;
+
+import java.util.ArrayList;
+
+public interface SaladInterface {
+    double overallColorie();
+    ArrayList<Vegetable> filterVegetablesByCalorie(double min, double max);
+}
