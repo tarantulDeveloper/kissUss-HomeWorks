a3a73239d07ae157e5a1e765112e564106847a2b
darika123123 <96609893+darika123123@users.noreply.github.com>
2022-10-03 19:56:46 +0600

    committed by darika :green_heart:

--- src/main/java/InheritanceTasks/variant_B/task_16/Account.java
+++ src/main/java/InheritanceTasks/variant_B/task_16/Account.java
@@ -1,6 +1,6 @@
 package InheritanceTasks.variant_B.task_16;
 
-class Account implements Comparable<Account>{
+class Account implements Comparable<Account>, AccountInterface{
     private int accountNumber;
     private double accountBalance;
     private boolean isEnabled;
@@ -10,29 +10,29 @@ class Account implements Comparable<Account>{
         this.accountBalance = accountBalance;
         this.isEnabled = isEnabled;
     }
-
+    @Override
     public int getAccountNumber() {
         return accountNumber;
     }
-
+    @Override
     public void setAccountNumber(int accountNumber) {
         this.accountNumber = accountNumber;
     }
 
-
+    @Override
     public double getAccountBalance() {
         return accountBalance;
     }
-
+    @Override
     public void setAccountBalance(double accountBalance) {
         this.accountBalance = accountBalance;
     }
-
+    @Override
     public String blockAccount(){
         this.isEnabled = false;
         return "Blocked";
     }
-
+    @Override
     public String unblockAccount() {
         this.isEnabled = true;
         return "Unblocked";
---
+++ src/main/java/InheritanceTasks/variant_B/task_16/AccountInterface.java
@@ -0,0 +1,10 @@
+package InheritanceTasks.variant_B.task_16;
+
+public interface AccountInterface {
+    int getAccountNumber();
+    void setAccountNumber(int accountNumber);
+    double getAccountBalance();
+    void setAccountBalance(double accountBalance);
+    String blockAccount();
+    String unblockAccount();
+}
